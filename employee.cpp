d0de65bdb5f4121fad0b76a75fcd1e72d71252f6
Snehadangare31 <163710095+Snehadangare31@users.noreply.github.com>
2024-12-28 20:21:09 +0530

    Add files via upload

--- employee.cpp
+++ employee.cpp
@@ -1,43 +1,64 @@
-#include<iostream>
-#include<iomanip>
-#include<cmath>
+#include <windows.h>
+#include <iostream>
+
 using namespace std;
-struct employee
+
+class employee
 {
- char name[100],id[100],des[100],mob[100];
-    float bs,da,hra,ts;
-}e1,e2,e3;
-void display(struct employee e);
+	int   emp_number;
+	char  emp_name[20];
+	float emp_basic;
+	float emp_da;
+	float emp_it;
+	float emp_net_sal;
+
+	public:
+
+		void get_emp_details();
+		float find_net_salary(float basic, float da, float it);
+		void show_emp_details();
+};
+
+void employee :: get_emp_details()
+{
+	cout<<"\nEnter employee number: ";
+	cin>>emp_number;
+	cout<<"\nEnter employee name: ";
+	cin>>emp_name;
+	cout<<"\nEnter employee basic: ";
+	cin>>emp_basic;
+	cout<<"\nEnter employee DA: ";
+	cin>>emp_da;
+	cout<<"\nEnter employee IT: ";
+	cin>>emp_it;
+}
+
+float employee :: find_net_salary(float basic, float da, float it)
+{
+    return (basic+da)-it;
+}
+
+void employee :: show_emp_details()
+{
+	cout<<"\n\n**** Details of  Employee ****";
+	cout<<"\nEmployee Name      :  "<<emp_name;
+	cout<<"\nEmployee number    :  "<<emp_number;
+	cout<<"\nBasic salary       :  "<<emp_basic;
+	cout<<"\nEmployee DA        :  "<<emp_da;
+	cout<<"\nIncome Tax         :  "<<emp_it;
+	cout<<"\nNet Salary         :  "<<find_net_salary(emp_basic, emp_da, emp_it);
+	cout<<"\n-------------------------------\n\n";
+}
+
+
 int main()
 {
-    struct employee e;
-    cout<<"Enter name of employee :"<<endl;
-    cin>>e1.name>>e2.name>>e3.name;
-    cout<<"Enter employee id:"<<endl;
-    cin>>e1.id>>e2.id>>e3.id;
-    cout<<"enter employee mobile number:"<<endl;
-    cin>>e1.mob>>e2.mob>>e3.mob;
-    cout<<"enter employee designation:"<<endl;
-    cin>>e1.des>>e2.des>>e3.des;
-    cout<<"enter basic salary"<<endl;
-    cin>>e1.bs>>e2.bs>>e3.bs;
-    e1.da=(80*e1.bs)/100;
-    e2.da=(80*e2.bs)/100;
-    e3.da=(80*e3.bs)/100;
-    e1.hra=(20*e1.bs)/100;
-    e2.hra=(20*e2.bs)/100;
-    e3.hra=(20*e3.bs)/100;
-    e1.ts=e1.bs+e1.da+e1.hra;
-    e2.ts=e2.bs+e2.da+e2.hra;
-    e3.ts=e3.bs+e3.da+e3.hra;
-    display(e);
+    employee emp;
+    
+    emp.get_emp_details();
+    emp.show_emp_details();
+
+    return 0;
 }
-void display(struct employee e)
- {
-  cout<<"employee details:"<<endl;
-  cout<<e1.name<<setw(5)<<e1.id<<setw(5)<<e1.mob<<setw(5)<<e1.des<<setw(5)<<e1.bs<<endl;
-  cout<<e2.name<<setw(5)<<e2.id<<setw(5)<<e2.mob<<setw(5)<<e2.des<<setw(5)<<e2.bs<<endl;
-  cout<<e3.name<<setw(5)<<e3.id<<setw(5)<<e3.mob<<setw(5)<<e3.des<<setw(5)<<e3.bs<<endl;
-  cout<<endl;
-}
- 
+
+
